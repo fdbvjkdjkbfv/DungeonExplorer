6c4af0e0bd1f3d40c3368ec3f52b8d77ff629a9b
fdbvjkdjkbfv <colossal.lego@gmail.com>
2025-04-24 18:38:08 +0100

    Updated the game to use OOP elements more so it is more efficient

--- Player.cs
+++ Player.cs
@@ -6,16 +6,19 @@ namespace DungeonExplorer
     {
         public static string Name { get; private set; }
         public  static int Health { get; private set; }
+        public static int Damage { get; private set; }
+
         private List<string> inventory = new List<string>();
 
-        public Player(string name, int health) 
+        public Player(string name, int health, int damage) 
         {
             Name = name;
             Health = health;
+            Damage = damage;
         }
         public void PickUpItem(string item)
         {
-
+            inventory.Add(item);
         }
         public string InventoryContents()
         {
--- Room.cs
+++ Room.cs
@@ -3,15 +3,27 @@
     public class Room
     {
         private string description;
+        private bool IsLocked;
 
-        public Room(string description)
+
+        public Room(string description, bool islocked)
         {
             this.description = description;
+            this.IsLocked = islocked;
         }
 
         public string GetDescription()
         {
             return description;
+        }
+
+        public bool GetIsLocked()
+        {
+            return IsLocked;
+        }
+        public void SetIsLocked(bool value)
+        {
+            IsLocked = value;
         }
     }
 }
