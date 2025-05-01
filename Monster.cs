7f2de588a0d1cd74d5efa07adb048337a39edf36
fdbvjkdjkbfv <colossal.lego@gmail.com>
2025-05-01 06:44:13 +0100

    try number 2.

---
+++ Monster.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DungeonExplorer
+{
+    public class Monster : Creature
+    {
+        public int AttackBonus;
+        public string DeathMessage;
+
+        public Monster(string name, int health, int damage, int armourClass, int attackBonus, string deathMessage) : base(name, health, damage, armourClass) 
+        {
+            AttackBonus = attackBonus;
+            DeathMessage = deathMessage;
+        }
+        public int GetAttackBonus()
+        {
+            return AttackBonus;
+        }
+        public override string SayDeathMessage()
+        {
+            return DeathMessage;
+        }
+    }
+}
--- Testing.cs
+++ Testing.cs
@@ -1,9 +1,34 @@
 ﻿using System;
 namespace DungeonExplorer
-
-public class Class1
 {
-	public Class1()
-	{
-	}
+    public class Testing
+    {
+        bool SuitableName = false;
+        bool TriedChangedLater = false;
+        string EnteredName = "";
+        public string SetName()
+        {
+            while (SuitableName == false)
+            {
+                Console.Write("enter your name (it cannot be changed later): ");
+                EnteredName = Console.ReadLine();
+                if (EnteredName == "changed later")
+                {
+                    Console.WriteLine("you are hilarious, try again");
+                    TriedChangedLater = true;
+                    SetName();
+                }
+                else if (EnteredName == "again" && TriedChangedLater == true)
+                {
+                    Console.WriteLine("hysterical, go again");
+                    SetName();
+                }
+                else
+                {
+                    SuitableName = true;
+                }
+            }
+            return EnteredName;
+        }
+    }
 }
